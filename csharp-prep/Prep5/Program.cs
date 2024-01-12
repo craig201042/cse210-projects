718b36656250ffcaf2be68f1ead09a318ce52e93
craig201042 <walila12@yahoo.com.tw>
2024-01-13 05:29:16 +0800

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name=PromptUserName();
+        int number=PromptUserNumber();
+        Double square=SquareNumber(number);
+        DisplayResult(name,square);             
+    }
+     static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name:");
+        string name=Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number:");
+        int number=int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static Double SquareNumber(int number)
+    {
+        Double square=Math.Pow(number,2);
+        return square;
+    }
+
+    static void DisplayResult(string name, Double square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
+   

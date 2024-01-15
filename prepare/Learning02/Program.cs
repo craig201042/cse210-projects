9e591c413bd4d47b213c94b955895375768aa571
craig201042 <walila12@yahoo.com.tw>
2024-01-16 05:32:59 +0800

    program modification

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1=new Job();
+        Job job2=new Job();
+        job1._jobTitle="Social worker";
+        job1._company="Charity foundation";
+        job1._startYear=2010;
+        job1._endYear=2011;
+
+        job2._jobTitle="QC Engineer";
+        job2._company="Sinbon";
+        job2._startYear=2011;
+        job2._endYear=2013;
+
+        Resume myResume=new Resume();
+        myResume._name="Craig Zeng";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }

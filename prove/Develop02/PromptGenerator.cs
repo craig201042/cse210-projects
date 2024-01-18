3447e0d88bf6560708bf60e7752fbf5490e7ad9b
craig201042 <walila12@yahoo.com.tw>
2024-01-19 06:08:09 +0800

    add one item

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -8,23 +8,28 @@ public class PromptGenerator{
          _prompts.Add("How did I see the hand of the Lord in my life today?");
          _prompts.Add("What was the strongest emotion I felt today?");
          _prompts.Add("If I had one thing I could do over today, what would it be?");
+         _prompts.Add("What breakfast would I like to eat tomorrow?");
+
           
           Random rnd=new Random();
-          int next=rnd.Next(5);
+          int next=rnd.Next(_prompts.Count);
           if(next==0){
-             _text=_prompts[0];
+             _text=_prompts[next];
           }
           else if(next==1){
-             _text=_prompts[1];
+             _text=_prompts[next];
           }          
           else if(next==2){
-             _text=_prompts[2];
+             _text=_prompts[next];
           }
           else if(next==3){
-             _text=_prompts[3];
+             _text=_prompts[next];
           }
-          else {
-             _text=_prompts[4];
+          else if(next==4) {
+             _text=_prompts[next];
+          }
+          else if(next==5){
+              _text=_prompts[next];
           }
           return _text;         
    }

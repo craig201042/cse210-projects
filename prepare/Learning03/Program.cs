6e81836fea7c4fe8618cdffbcfcccb30207e2e19
craig201042 <walila12@yahoo.com.tw>
2024-01-22 16:45:09 +0800

    main modification

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1=new Fraction();
+        Fraction f2=new Fraction(5);
+        Fraction f3=new Fraction(5,2);
+        f1.SetTop(7);
+        f1.GetTop();
+        f2.SetBottom(3);
+        f2.GetBottom();
+        f3.SetTop(8);
+        f3.GetTop();
+        f3.SetBottom(3);
+        f3.GetBottom();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
     }
 }

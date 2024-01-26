8fdc1d2986fca15fc700a741a0b4f12bb3f39bcf
craig201042 <walila12@yahoo.com.tw>
2024-01-26 15:29:09 +0800

    script

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,47 @@
+
+public class Scripture{
+    private Reference _reference;
+    private List<Word> _words=new List<Word>(); 
+    private Word w=new Word("text");
+    private List<string> s;
+
+    private bool _IsCompletelyHidden;
+    private string str=" ";
+
+    public Scripture(Reference reference,string text){
+             _reference=reference;
+             s=text.Split(',').ToList();
+             for(int i=0;i<s.Count;i++){
+                 w.SetText(s[i]);
+                _words.Add(w);
+             }
+    }
+    public void HideRandomWords(int numberToHide){
+           Random rnd=new Random();         
+           numberToHide=rnd.Next(_words.Count);
+           _words[numberToHide].Show();
+           Console.ReadLine();
+           Console.Clear();
+           _words[numberToHide].Hide();
+    }
+    public string GetTheDispaly(){ 
+          foreach(Word w in _words){
+            str=w.GetText();
+            s.Add(str);
+          }
+          string s1=string.Join(" ", s.ToArray());
+          return _reference.GetDispalyText()+s1;
+    }
+    public bool IsCompletelyHidden(){
+          foreach(Word w in _words){
+            while(w.IsHidden()==true){
+                _IsCompletelyHidden=true;
+            }
+            _IsCompletelyHidden=false;
+          }      
+          return _IsCompletelyHidden;                  
+    }
+
+}
+
+

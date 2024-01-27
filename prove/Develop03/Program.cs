e2efa2dc0b16dc72d653671f8b751a0d5bfdfdf7
craig201042 <walila12@yahoo.com.tw>
2024-01-27 15:33:07 +0800

    main 05

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,19 +5,22 @@ class Program
 {
     private static bool hide;
     private static string userInput;
+    private static List<string> sl;
 
     static void Main(string[] args)
     {
         Reference  reference= new Reference("John", 3, 16, 17);
-        string content="For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.\rFor God did not send his Son into the world to condemn the world, but to save the world through him.";
+        string content="For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.For God did not send his Son into the world to condemn the world, but to save the world through him.";
+        sl=content.Split(',').ToList();
         Scripture script = new Scripture(reference, content);
+        Random rnd=new Random();
         Console.WriteLine(script.GetTheDispaly());
         Console.WriteLine("Press enter to continue or type 'Quit' to finish");
         userInput=Console.ReadLine();
         while(hide!=true){
             if(userInput!="Quit"){
              for(int i=0;i<3;i++){
-                script.HideRandomWords(content.Length);
+                script.HideRandomWords(rnd.Next(sl.Count));
              }
             }
             else{

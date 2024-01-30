4d4cb85d30ec3b95db30027ebb5bafd773c0cc5b
craig201042 <walila12@yahoo.com.tw>
2024-01-30 17:11:57 +0800

    main

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+       Assignment assign=new Assignment("Craig", "Math");
+       string str=assign.GetSummary();
+       Console.WriteLine(str);
+            
+       MathAssignment mathassign=new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+       Console.WriteLine(mathassign.GetSummary());
+       Console.WriteLine(mathassign.GetHomeworkList());
+       
+       WritingAssignment writingassign=new WritingAssignment("Mary Waters", "Chinese History", "The Causes of World War II");
+
+       Console.WriteLine(writingassign.GetSummary());
+       Console.WriteLine(writingassign.GetWritingInformation());
+
+
     }
 }

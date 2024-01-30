22c214bfcdfe2dad2d44e37ee7f685a713e320ac
craig201042 <walila12@yahoo.com.tw>
2024-01-30 17:15:11 +0800

    main06

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,30 +5,24 @@ class Program
 {
     private static bool hide=false;
     private static string userInput;
-    private static List<string> sl;
 
     static void Main(string[] args)
     {
         Reference  reference= new Reference("John", 3, 16, 17);
         string content="For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.For God did not send his Son into the world to condemn the world, but to save the world through him.";
-        sl=content.Split(',').ToList();
         Scripture script = new Scripture(reference, content);
         Random rnd=new Random();
-        Console.WriteLine(script.GetTheDispaly());
-        Console.WriteLine("Press enter to continue or type 'Quit' to finish");
-        userInput=Console.ReadLine();
-        while(userInput!="Quit"){
-            if(hide!=true){
-             for(int i=0;i<3;i++){
-                script.HideRandomWords(rnd.Next(sl.Count));
-             }
-             Console.Clear();
-             Console.WriteLine(script.GetTheDispaly());
-             Console.WriteLine("Press enter to continue or type 'Quit' to finish");
-             userInput=Console.ReadLine();
+        
+        while(hide!=true){
+            Console.Clear();
+            Console.WriteLine(script.GetTheDispaly());
+            Console.WriteLine("Press enter to continue or type 'Quit' to finish");
+            userInput=Console.ReadLine();
+            if(userInput!="Quit"){
+             script.HideRandomWords(rnd.Next(1,5));
              hide=script.IsCompletelyHidden(); 
             }
-            else{
+            else {
                 break;
             }   
         }

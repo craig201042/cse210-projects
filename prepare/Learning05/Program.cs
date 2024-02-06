22395a9e32742efdae0ef46e0cd14890ae7fd6f7
craig201042 <walila12@yahoo.com.tw>
2024-02-06 17:02:17 +0800

    main 01

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.ComponentModel;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+       Square sq=new Square("brown", 4);
+       Console.WriteLine(sq.GetColor());
+       Console.WriteLine(sq.GetArea());
+
+       Rectangle rec=new Rectangle("black", 4, 3);
+       Circle cir=new Circle("red", 3);
+
+       List<Shape> shapes=new List<Shape>();
+       shapes.Add(sq);
+       shapes.Add(rec);
+       shapes.Add(cir);
+
+       foreach(Shape s in shapes){
+         Console.WriteLine(s.GetColor());
+         Console.WriteLine(s.GetArea());
+       }
     }
 }

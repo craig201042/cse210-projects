d0bf3f1c05bec78e7929f80b3762cc83d56ecc9a
craig201042 <walila12@yahoo.com.tw>
2024-02-11 00:55:10 +0800

    main01

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        GoalManager gm= new GoalManager();
+        gm.Start();
     }
 }

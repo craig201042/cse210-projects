02fdca8ec4b6415c9449cb2eb51bfb1c05f67c79
craig201042 <walila12@yahoo.com.tw>
2024-02-13 17:09:21 +0800

    address01

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,16 @@
+public class Address{
+    private string _streetAddress;
+    private string _city;
+    private string _state;
+    private string _country;
+   
+    public Address(string streetAddress, string city, string state, string country){
+        _streetAddress=streetAddress;
+        _city=city;
+        _state=state;
+        _country=country;
+    }
+    public string GetAddress(){
+        return $"{_streetAddress},{_city}\n{_state},{_country}";
+    }
+}

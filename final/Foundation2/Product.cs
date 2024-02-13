1f8f8bc171cf10f4e3ea0e4f50b67be3b48e47d2
craig201042 <walila12@yahoo.com.tw>
2024-02-13 17:09:01 +0800

    product01

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,19 @@
+public class Product{
+    private string _name;
+    private string _id;
+    private double _price;
+    private int _quantity;
+    public Product(string name, string id, double price, int quantity){
+        _name=name;
+        _id=id;
+        _price=price;
+        _quantity=quantity;
+    }
+    public double totalCost(){
+        return _price*_quantity;
+    }
+    public void DisplayProductInfo(){
+        Console.WriteLine($"{_name}--{_id}");
+    }
+
+}
